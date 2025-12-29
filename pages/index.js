918114e021da890f955f3e0b5e210031c1604092
Lukas <lukassilvabernardi@outlook.com>
2025-12-29 16:25:11 -0300

    adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <h1>
+      "Três coisas não podem ser escondidas por muito tempo: o sol, a lua e a
+      verdade." <br /> - Buda
+    </h1>
+  );
+}
+
+export default Home;
